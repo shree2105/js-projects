ebb2d98a13eeaffabc959890f7739c33b344f363
Rajashree.k <156102749+shree2105@users.noreply.github.com>
2025-07-23 14:12:45 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-const btn=document.getElementById("colorBtn").addEventListener("click",function(){
-    let randomColor="#"+Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor=randomColor
-});
